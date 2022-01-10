f3aca8f058e4d3fc25ad988be54dba9c64201dce
Venelin-Bekyarov <venbeks@gmail.com>
2022-01-10 10:13:14 +0000

    upload

--- app/src/main/java/apprentice/hr/app/InitialLoginActivity.java
+++ app/src/main/java/apprentice/hr/app/InitialLoginActivity.java
@@ -1,7 +1,13 @@
 package apprentice.hr.app;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -20,6 +26,35 @@ public class InitialLoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial_login);
 
+        username = findViewById(R.id.username);
+        password = findViewById((R.id.password));
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (TextUtils.isEmpty(username.getText().toString()) || TextUtils.isEmpty(
+                        password.getText().toString())) {
+                    Toast.makeText(InitialLoginActivity.this, "Empty data provided",
+                            Toast.LENGTH_LONG).show();
+                } else if (username.getText().toString().equals(correct_username)) {
+                    if (password.getText().toString().equals(correct_password)) {
+                        Toast.makeText(InitialLoginActivity.this, "Success",
+                                Toast.LENGTH_LONG).show();
+
+                    } else {
+                        Toast.makeText(InitialLoginActivity.this,
+                                "Invalid Username and/or Password", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(InitialLoginActivity.this,
+                            "Invalid Username and/or Password", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
+
